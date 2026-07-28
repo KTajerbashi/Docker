64654ffce3204ecb2e6bdf63810b9745ce69bd66
KTajerbashi <kamrantajerbashi@gmail.com>
2026-07-28 23:45:10 +0330

    #Feature Add Dashboard Infrastructure Layer

---
+++ Dashboard/2.Infra/Dashboard.Infrastructure/DependencyInjections.cs
@@ -0,0 +1,14 @@
+﻿using Dashboard.Application;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dashboard.Infrastructure;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+
+        services.AddApplication();
+        return services;
+    }
+}

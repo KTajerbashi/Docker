209cd1fc0875878167e32982055e1ab1ea9bdf58
KTajerbashi <kamrantajerbashi@gmail.com>
2026-07-28 23:44:56 +0330

    #Feature Add Dashboard Application Layer

---
+++ Dashboard/1.Core/Dashboard.Application/DependencyInjections.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Dashboard.Application;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
